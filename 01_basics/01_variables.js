9743a036e133344224fdea34af3b222ba8718b70
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-07-02 06:14:20 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountID = 144553
+let accoutEmail =  "riddhi123@gmail.com"
+var acountPassword = "12345"
+accountCity = "bangalore"
+ let accountState;
+
+/* 
+prefer not to use var 
+because of issues in block scope and functional scope 
+*/
+
+accoutEmail="suhani2121@gmail.com"
+acountPassword = "43434"
+accountCity = "bengaluru"
+
+console.table ([accountID, accoutEmail, acountPassword, accountCity, accountState])
