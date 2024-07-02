d8f68080b44679251fb47f694523d028674b59e0
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-07-02 06:53:06 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "22"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"22" =>33
+//"22abc" => NaN
+// "true" => 1; false = > 0
+
+let isLoggedIn = "vividh"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log((booleanisLoggedIn));
+
+
+
+let someNumber = 44
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
