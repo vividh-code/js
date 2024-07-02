b67356f9dede34979ca2ab99f6c936cccf8df009
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-07-02 07:44:20 +0000

    datatypes

---
+++ 01_basics/datatypea-summary.js
@@ -0,0 +1,42 @@
+//Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIN = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('321')
+const anotherId = Symbol('321')
+
+// console.log(id === anotherId);
+
+const bigNumber = 3673737638437n
+
+
+//Refrence (NonPreitive)
+
+//Array, Objects, Functions
+
+const heros = ["riddhi", "vani", "ritika"]
+let myobj = {
+    name : "vividh",
+    age: "24",
+}
+     
+const myFunction = function(){
+    console.log("Hello riddhi");
+}
+
+console.log(typeof myFunction);
+
+
+
+
+
+
+
