65ce1ae215f8be016ca781886c44a9b1e432a543
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-07-08 19:32:17 +0000

    container

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,30 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log((myDate.toLocaleString()));
+// console.log(myDate.toISOString());
+// console.log(typeof myDate);
+
+
+// let myCreativeDate = new Date(2024, 0, 23)
+// let myCreativeDate = new Date(2024, 0, 23,5,4)
+
+// let myCreativeDate = new Date("2024-01-23");
+
+// console.log(myCreativeDate.toDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+
+let newDate = new date();
+console.log(newDate);
+console.log(newDate.getMonth());
+
+
+
+
+
+
+
+
+
