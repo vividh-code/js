752340c39556f448240941cc348c0c488579b504
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-07-31 06:26:00 +0000

    control

---
+++ 04_control_flow/one.js
@@ -0,0 +1,74 @@
+//
+// const isUserloggedIn= true
+// const tempreture =42
+
+// if (tempreture===41){
+//     console.log("less than 50");
+// }else{
+
+// console.log("tempreture is greater than 50");
+// }
+// console.log('executed');
+// <, >, <= ,>=, == ,!=, ===, !==
+
+// const score =200;
+// if(score >100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+const balance =1000
+
+// if(balance>500) console.log("test");
+// console.log("test2");
+
+// if(balance  < 500){
+//      console.log("less than 500");
+
+// }else if(balance < 750){
+//         console.log("less than 750");
+
+// }else if(balance < 900){
+//     console.log("less than 750");
+// }else {
+//         console.log("less than 1200");
+// }
+
+
+
+const userLoggedIn = true
+const debitcard = true 
+const userLoggedInFromGoogle= false
+const userLoggedInFromEmail= true
+
+if(userLoggedIn && debitcard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(userLoggedInFromGoogle || userLoggedInFromEmail){
+    console.log("User logged in");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
