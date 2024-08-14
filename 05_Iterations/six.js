da28a882caf08c6c2d202d00e805421724e400f7
vividh-code <160312866+vividh-code@users.noreply.github.com>
2024-08-14 07:17:03 +0000

    loop

---
+++ 05_Iterations/six.js
@@ -0,0 +1,15 @@
+// const coding = ["js", "python", "java", "jabascript", "ruby"]
+
+// const values = coding.forEach ((item) => {
+//     console.log(item);
+//     return  item 
+    
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNums = myNums.filter( (num) => num> 4)
+console.log(newNums);
+
